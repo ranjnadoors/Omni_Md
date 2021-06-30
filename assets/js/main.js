e50ea65c7a78ef28dda354e198a97f109c239324
ranjna <ranjna@doors4s.com>
2021-06-30 19:15:54 +0530

    menu left

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
-$(document).ready(function(){
-    
-      $("#home-text").slideUp();
-    
-   
-      $("#home-img").slideDown();
-   
-  });
+$(document).ready(function () {
+  $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      if ($(window).width() > 768) {
+          if (scroll > 300) {
+              $(".nav-position").css("background", "transparent" + "position", "fixed");
+          } else {
+              $(".nav-position").css("background", "transparent");
+          }
+      }
+  })
+
+  if ($(window).width() < 769) {
+      $('.nav-bg').css("background", "rgba(0, 0, 0, 0.5)");
+      $('..nav-item a').css("color","white");
+      $(window).scroll(function () {
+          var scrolls = $(window).scrollTop();
+          if (scrolls > 300) {
+              $(".nav-position").css("background", "rgba(0, 0, 0, 0.5)");
+              $('.nav-bg').css("background", "rgba(0, 0, 0, 0.0)" + "color" , "white")
+          }
+      })
+  }
+})
